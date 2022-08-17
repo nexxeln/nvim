af5ffc251fb02b93d277f736677776c921b3c9d5
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-17 16:16:05 +0530

    remove dw keymap

--- lua/maps.lua
+++ lua/maps.lua
@@ -6,9 +6,6 @@ keymap.set('n', 'x', '"_x')
 keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
 
--- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
-
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -72,8 +72,14 @@ nvim_lsp.flow.setup {
   capabilities = capabilities
 }
 
+nvim_lsp.denols.setup {
+  on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("package.json"),
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities
