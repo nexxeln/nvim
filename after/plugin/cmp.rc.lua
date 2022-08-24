2fb251ebb156d2561896a7c909af9ddb272169cb
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-25 00:17:46 +0530

    add tab shift tab bindings for autocomplete

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -15,9 +15,28 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
-            select = true
+            select = true,
         }),
-    }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.fn["vsnip#available"](1) == 1 then
+            feedkey("<Plug>(vsnip-expand-or-jump)", "")
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+            feedkey("<Plug>(vsnip-jump-prev)", "")
+          end
+        end, { "i", "s" }),
+      }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'buffer' },
