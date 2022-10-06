22a88d95b732657e62ad1778dfb0d56af75c3357
Shoubhit Dash <shoubhit2005@gmail.com>
2022-10-06 15:46:58 +0530

    feat: add impatient to improve startup time

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require("impatient")
 require("options")
 require("plugins")
 require("maps")
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -121,6 +121,11 @@ _G.packer_plugins = {
     path = "/home/nxl/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
+  ["impatient.nvim"] = {
+    loaded = true,
+    path = "/home/nxl/.local/share/nvim/site/pack/packer/start/impatient.nvim",
+    url = "https://github.com/lewis6991/impatient.nvim"
+  },
   ["indent-blankline.nvim"] = {
     loaded = true,
     path = "/home/nxl/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
