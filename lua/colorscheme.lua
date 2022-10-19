bf9eff8d6f9d8d9864a0da26e27a71ab4286e1a1
Shoubhit Dash <shoubhit2005@gmail.com>
2022-10-19 23:51:18 +0530

    feat: back to the old stuff

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -8,15 +8,15 @@ kanagawa.setup({
 	commentStyle = { italic = false },
 	keywordStyle = { italic = false },
 	variablebuiltinStyle = { italic = false },
-	-- transparent = true,
+	transparent = true,
 })
 
--- vim.cmd("colorscheme kanagawa")
+vim.cmd("colorscheme kanagawa")
 
-local no_clown_present, no_clown_fiesta = pcall(require, "no-clown-fiesta")
-
-if not no_clown_present then
-	return
-end
-
-no_clown_fiesta.setup({})
+-- local no_clown_present, no_clown_fiesta = pcall(require, "no-clown-fiesta")
+--
+-- if not no_clown_present then
+-- 	return
+-- end
+--
+-- no_clown_fiesta.setup({})
