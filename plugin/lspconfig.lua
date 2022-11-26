d71d043cbeca286bb373b6448c0ca2f3450af435
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-26 23:20:24 +0530

    feat: svelte lsp

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -100,6 +100,8 @@ nvim_lsp.rust_analyzer.setup({})
 
 nvim_lsp.astro.setup({})
 
+nvim_lsp.svelte.setup({})
+
 nvim_lsp.prismals.setup({})
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
